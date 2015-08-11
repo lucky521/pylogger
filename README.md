0741ce1cb62e65d164b953f6ba44bc8e49242ff6
Lucky <307479836@qq.com>
2015-08-11 22:51:04 +0800

    add

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 1. log for function call.
 2. log where you want.
+
+
+### Installation
+python setup.py install
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pylogger',
+      version='1.0',
+      description='Python Logger Utilities',
+      author='Lucky',
+      author_email='',
+      url='https://github.com/lucky521',
+      packages=[],
+     )
